0c153662b1ebe0578337b30ee884758e94e5c401
Jasir <k250701@nu.edu.pk>
2025-10-13 10:39:56 +0500

    Add files via upload

---
+++ Answer 1.c
@@ -0,0 +1,47 @@
+/*
+Take an array of 12 employee IDs. Write a program that checks if a given employee ID exists in the array or not
+*/
+
+#include <stdio.h>
+
+int main(){
+	
+	int total_employees = 5;
+	int EmployeeID[total_employees];
+	int employee;
+	int check_ID;
+	int present = 0;
+	
+	for (employee = 0; employee < total_employees; employee++){
+		
+		printf("Enter employee ID for employee number %d\n", employee + 1);
+		scanf("%d", &EmployeeID[employee]);
+	}
+	
+//	for (employee = 0; employee < total_employees; employee++){
+//		
+//		printf("%d", EmployeeID[employee]);
+//	}
+//	
+
+
+	printf("\n==========================\n");
+	printf("Enter a employeeID to check:\n");
+	scanf(" %d", &check_ID);
+	
+	for (employee = 0; employee < total_employees; employee++){
+		
+		if (EmployeeID[employee] == check_ID){
+			present = 1;
+		}
+	}
+	
+	if (present == 0) {
+		printf("The given ID does not exist");
+	}else{
+		printf("The given ID exists");
+	}
+	
+	
+	return 0;
+}
---
+++ Answer 2.c
@@ -0,0 +1,30 @@
+/*
+A teacher has marks of 10 students stored in an array. Write a program to calculate the average marks. (Take input from the user)
+*/
+
+#include <stdio.h>
+
+int main() {
+	int total_students = 10;
+	int marks[10];
+	int student;
+	int total_marks = 0;
+	float average; 
+	
+	for(student=0; student < total_students; student++){
+		printf("Enter the marks of student %d out of %d: ", student + 1, total_students);
+		scanf(" %d", &marks[student]);
+	}
+	
+	for(student=0; student < total_students; student++){
+		total_marks += marks[student];
+	}
+	
+	
+	average = total_marks / total_students;
+	printf("The average marks are: %.2f", average);
+	
+	
+	
+	return 0;
+}
